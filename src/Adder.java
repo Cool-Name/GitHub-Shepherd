cd9628a3fab2f32a0955f9f19e030328dc9465d4
hvvdb1 <hvvdb1@students.waikato.ac.nz>
2016-08-02 23:15:27 +1200

    Cleaned up and added func to committer and adder

--- src/Adder.java
+++ src/Adder.java
@@ -7,43 +7,58 @@ import org.eclipse.jgit.api.errors.NoFilepatternException;
 import org.eclipse.jgit.internal.storage.file.FileRepository;
 import org.eclipse.jgit.lib.Repository;
 
-
 public class Adder {
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		String direc = args[0];
 		String fileName = args[1];
 		try {
 			Repository r = new FileRepository(direc);
 			Git g = new Git(r);
-			
+
 			Adder adr = new Adder();
 			adr.addFile(g, fileName);
-			
+
+			g.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 			return;
 		}
 	}
-	
+
 	// This will probs end up being static
-	private void addFile(Git g, String fileName)
-	{
-		File myFile = new File(g.getRepository().getDirectory().getParent(), fileName);
-		
+	public void addFile(Git g, String fileName) {
+		File myFile = new File(g.getRepository().getDirectory().getParent(),
+				fileName);
+
 		try {
 			myFile.createNewFile();
 		} catch (IOException e) {
 			e.printStackTrace();
 			return;
 		}
-		
+
 		try {
 			g.add().addFilepattern(fileName).call();
-			
-			System.out.println("Added file " + myFile + " to repository at " + g.getRepository().getDirectory());
-			
+
+			System.out.println("Added file " + myFile + " to repository at "
+					+ g.getRepository().getDirectory());
+
+		} catch (NoFilepatternException e) {
+			e.printStackTrace();
+		} catch (GitAPIException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void addAll(Git g) {
+
+		try {
+			g.add().addFilepattern(".").call();
+
+			System.out.println("Added all to repository at "
+					+ g.getRepository().getDirectory());
+
 		} catch (NoFilepatternException e) {
 			e.printStackTrace();
 		} catch (GitAPIException e) {
--- src/Committer.java
+++ src/Committer.java
@@ -1,6 +1,8 @@
 import java.io.File;
 
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.NoFilepatternException;
 import org.eclipse.jgit.internal.storage.file.FileRepository;
 import org.eclipse.jgit.lib.Repository;
 
@@ -26,6 +28,72 @@ public class Committer {
             
             g.close();
 		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void commitSingle(Git g, String fileName, String direc, String message)
+	{
+		File myfile = new File(g.getRepository().getDirectory().getParent(), fileName);
+        try {
+			myfile.createNewFile();
+			
+        //TODO set all options - - SNORE
+        g.add().addFilepattern(fileName).call();
+        g.commit().setMessage(message).call();
+        
+        g.close();
+        } catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void commitSingle(Git g, String fileName, String direc, String message, boolean amend)
+	{
+		File myfile = new File(g.getRepository().getDirectory().getParent(), fileName);
+        try {
+			myfile.createNewFile();
+			
+        //TODO set all options - - SNORE
+        g.add().addFilepattern(fileName).call();
+        g.commit().setMessage(message).setAmend(amend).call();
+        
+        g.close();
+        } catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void commitAll(Git g)
+	{
+		try {
+			//g.add().addFilepattern(".").call();
+			
+			//g.commit().setMessage("Committing all").call();
+			
+			g.commit().setAll(true).setMessage("Committing all").call();
+		} catch (NoFilepatternException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (GitAPIException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void commitAll(Git g, String message)
+	{
+		try {
+			//g.add().addFilepattern(".").call();
+			
+			//g.commit().setMessage("Committing all").call();
+			
+			g.commit().setAll(true).setMessage(message).call();
+		} catch (NoFilepatternException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (GitAPIException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
