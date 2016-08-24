dc7822bb677da48a1625d38197105622eaea2ca7
Ben <b@en.vc>
2016-08-24 23:02:07 +1200

    Add server button now adds a string to the server list

--- src/MyController.java
+++ src/MyController.java
@@ -11,13 +11,19 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 
 public class MyController implements Initializable
 {
-	
+	//declares fxml controls using their id
 	@FXML Button listRepoBtn;
+	@FXML Button addServer;
+	@FXML TextField serverTextField;
+	@FXML TreeView<String> serverList;
 	@FXML TableView<Row> data_table;
 	@FXML TableColumn<Row, String> enabled;
 	@FXML TableColumn<Row, String> repositories;
@@ -25,6 +31,9 @@ public class MyController implements Initializable
 	@FXML TableColumn<Row, String> latest_version;
 	@FXML TableColumn<Row, String> last_pulled;
 	@FXML TableColumn<Row, String> description;
+	
+	
+	TreeItem<String> root;
 	
 	private final ObservableList<String> repoList = FXCollections.observableArrayList(
     		new String("A"),
@@ -38,9 +47,11 @@ public class MyController implements Initializable
 				new Row("yes", "Test Repo","4.1","5.0", "5:00pm 10/10/2016", "This is a sample description")
 	           );
 		
+	 
+		
 		            	
 	@FXML 
-	private void listRepos()
+	private void ListRepos()
 	{
 		enabled.setCellValueFactory(
 		        new PropertyValueFactory<Row,String>("enabled"));
@@ -57,12 +68,38 @@ public class MyController implements Initializable
 
 		
 		data_table.setItems(observableList);
-		System.out.println("IT WORKED!!");
+	
+	}
+	
+	
+	@FXML 
+	private void ShowContextMenu()
+	{
+				
+	}
+	
+	@FXML 
+	private void AddServer()
+	{
+		 //checks if the search box is empty 	
+		 if (serverTextField.getText() != null && !serverTextField.getText().isEmpty())
+		 {
+				//adds the server to the server list
+			 	TreeItem<String> item = new TreeItem<>(serverTextField.getText());
+			    root.getChildren().add(item);
+		   	    serverList.setRoot(root);
+		   	    
+		   	    //clears the text from the server textField
+		   	    serverTextField.clear();
+		 }
 	}
 	
 	@Override
 	public void initialize(URL fxmlFileLocation, ResourceBundle resources)
 	{
-
+		//adds the root item servers
+		root = new TreeItem<String>("Servers");
+		root.setExpanded(true);
+		serverList.setRoot(root);    
 	}
 }
